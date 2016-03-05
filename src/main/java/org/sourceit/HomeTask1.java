e65b392398190bcf478718d26ec3a21c24ddc030
Aligator <lbvfrjhjkm1@mail.ru>
2016-03-05 11:48:36 +0200

    all methods

--- src/main/java/org/sourceit/HomeTask1.java
+++ src/main/java/org/sourceit/HomeTask1.java
@@ -1,7 +1,9 @@
 package org.sourceit;
 
 public class HomeTask1 {
-
+    public static void main(String[] args) {
+        System.out.println(isEven(1));
+    }
     /**
      * Проверить, является ли число четным.
      *
@@ -9,7 +11,12 @@ public class HomeTask1 {
      * @return является ли число четным.
      */
     public static boolean isEven(int number) {
-        return false;
+        if(number>0) {
+            boolean b = (number % 2 == 0 ? true : false);
+            return b;
+        }else{
+            return false;
+        }
     }
 
     /**
@@ -22,7 +29,12 @@ public class HomeTask1 {
      * @return значение гипотенузы.
      */
     public static double findHypotenuse(double a, double b) {
-        return 0D;
+        if((a>0)&&(b>0)){
+            double c=Math.sqrt(Math.pow(a,2)+Math.pow(b,2));
+            return c;
+        }else{
+            return 0;
+        }
     }
 
     /**
@@ -34,7 +46,12 @@ public class HomeTask1 {
      * @return периметр треугольника.
      */
     public static double perimeter(double a, double b, double c) {
-        return 0D;
+        if(a>0&b>0&c>0){
+            double p= a+b+c;
+            return p;
+        }else{
+            return 0;
+        }
     }
 
     /**
@@ -46,7 +63,12 @@ public class HomeTask1 {
      * @return площадь треугольника.
      */
     public static double area(double a, double b) {
-        return 0D;
+        if(a>0&&b>0){
+            double s=(a*b)/2;
+            return s;
+        }else{
+            return 0;
+        }
     }
 
     /**
@@ -57,7 +79,17 @@ public class HomeTask1 {
      * @return сгенерированное число.
      */
     public static int generateNumberFromRange(int min, int max) {
-        return 0;
+        if(min<max){
+            int y=max-min;
+            int g=min+((int)(Math.random()*(y+1)));
+            return g;
+        }else if(min>max){
+            int y=min-max;
+            int g=max+((int)(Math.random()*(y+1)));
+            return g;
+        }else{
+            return min;
+        }
     }
 
     /**
@@ -69,7 +101,13 @@ public class HomeTask1 {
      * @return сумма цифр.
      */
     public static long calculateSum(long number) {
-        return 0L;
+        number=(number<0? number*-1:number);
+        int sum=0;
+        while (number>0){
+            sum+=number%10;
+            number/=10;
+        }
+        return sum;
     }
 
     /**
@@ -81,9 +119,23 @@ public class HomeTask1 {
      * @return элемент последовательности.
      */
     public static int fibonacci(int till) {
-        return 0;
-    }
+        int a1=1;
+        int a2=1;
+        int f=0;
+        for(int i=3;i<=till;i++){
+            f=0;
+            f=a1+a2;
+            a1=a2;
+            a2=f;
+        }
+        if((till<3)&(till>0)){
+            f=a1;
+        }else if(till<0){
+            f=0000;
+        }
 
+        return f;
+    }
     /**
      * "Счастливым" считается билетик у которого
      * сумма первых трёх цифр номера равна сумме последних
@@ -93,6 +145,22 @@ public class HomeTask1 {
      * @return является ли билет счастливым.
      */
     public static boolean isHappy(long ticket) {
-        return false;
+        int left=0;
+        int right=0;
+        for(int i=1;i<=6;i++){
+            if (i<4){
+                right+= ticket%10;
+                ticket/=10;
+            }
+            if (i>=4){
+                left+= ticket%10;
+                ticket/=10;
+            }
+        }
+        if(left==right){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
