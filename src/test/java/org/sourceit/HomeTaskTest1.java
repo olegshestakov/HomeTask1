ea64ae2ce69dc2f2ac5c58e8ed5c485e1822fca1
Dmitrii Bondar <lbvfrjhjkm@mail.ru>
2016-06-01 20:57:26 +0300

    all methods 1

--- src/test/java/org/sourceit/HomeTaskTest1.java
+++ src/test/java/org/sourceit/HomeTaskTest1.java
@@ -6,7 +6,7 @@ import org.testng.Assert;
 public class HomeTaskTest1 {
 
     /**
-     * Tests on isEven method1-
+     * Tests on isEven method1123
      */
     @Test
     public void testIsEvenOnOdd() {
